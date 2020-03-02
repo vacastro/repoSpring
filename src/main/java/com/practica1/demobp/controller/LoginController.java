4e067b2e366990b0132eeb6c461beff4414ca584
Florpau <florpaumont@gmail.com>
2020-03-02 19:13:45 -0300

    se agregan validaciones a login

--- src/main/java/com/practica1/demobp/controller/LoginController.java
+++ src/main/java/com/practica1/demobp/controller/LoginController.java
@@ -2,9 +2,13 @@ package com.practica1.demobp.controller;
 
 import com.practica1.demobp.model.Usuario;
 import com.practica1.demobp.repository.UsuarioJpaRepository;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,33 +17,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("login")
 public class LoginController {
 
-    @Autowired
-    UsuarioJpaRepository usuarioJpaRepository;
+	@Autowired
+	UsuarioJpaRepository usuarioJpaRepository;
 
-    @GetMapping()
-    public String irAlLogin() {
+	@GetMapping()
+	public String irAlLogin(Usuario usuario) {
 
-        return "login";
-    }
+		return "login";
+	}
 
-    @PostMapping()
-    public String comprobarLogin(Model model, Usuario usuario) {
+	@PostMapping()
+	public String comprobarLogin(Model model, @Valid Usuario usuario, BindingResult result) {
+		if (result.hasErrors()) {
+			return "login";
+		}
 
-        String mailIngresado = usuario.getEmail();
-        String contraseniaIngresada = usuario.getContrasenia();
+		String mailIngresado = usuario.getEmail();
+		String contraseniaIngresada = usuario.getContrasenia();
 
-        Usuario usuarioEncontrado = usuarioJpaRepository.findByEmail(mailIngresado);
+		Usuario usuarioEncontrado = usuarioJpaRepository.findByEmail(mailIngresado);
 
-        if(usuarioEncontrado == null) {
-            return "login";
-        }
+		if (usuarioEncontrado == null) {
+			return "login";
+		}
 
-        if(usuarioEncontrado.getContrasenia().equals(contraseniaIngresada)) {
-            model.addAttribute("email", usuarioEncontrado.getEmail());
-            return "index";
-        } else {
-            return "login";
-        }
+		if (usuarioEncontrado.getContrasenia().equals(contraseniaIngresada)) {
+			model.addAttribute("email", usuarioEncontrado.getEmail());
+			return "index";
+		} else {
+			return "login";
+		}
 
-    }
+	}
 }

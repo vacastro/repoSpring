7ba4cda9b833e5eacc324d1383e8bf9c6b8f0551
MariaFer <mariaf.arenas@hotmail.com>
2020-03-02 17:31:51 -0300

    Model, repository y controller de carrito

---
+++ src/main/java/com/practica1/demobp/model/Carrito.java
@@ -0,0 +1,48 @@
+package com.practica1.demobp.model;
+
+import javax.persistence.*;
+
+@Entity
+
+public class Carrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column (nullable = false)
+    private Usuario usuario;
+    @Column (nullable = false)
+    private Producto producto;
+
+    public Carrito() {
+    }
+
+    public Carrito(Integer id, Usuario usuario, Producto producto) {
+        this.id = id;
+        this.usuario = usuario;
+        this.producto = producto;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}

16f2bc4f17246cee91ccaf99b3482b3000fa7d24
MariaFer <mariaf.arenas@hotmail.com>
2020-03-03 23:49:38 -0300

    se modificaron detalles en base de datos para la tabla ABM productos, y vistas relacionadas

--- src/main/java/com/practica1/demobp/controller/ProductoController.java
+++ src/main/java/com/practica1/demobp/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.practica1.demobp.controller;
 import java.util.List;
 
 
+import com.practica1.demobp.model.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.practica1.demobp.model.Producto;
 import com.practica1.demobp.repository.ProductoJpaRepository;
+import org.thymeleaf.model.IModel;
 
 import javax.jws.WebParam;
 
@@ -21,10 +23,15 @@ import javax.jws.WebParam;
 public class ProductoController {
 	@Autowired
 	private ProductoJpaRepository productoJpaRepository;
-	
+
+	//quiero inyectar CategoriaJpaRepository por el Autowired, pero me sugiere colocarlo como parámetro en la public
+	//class de ProductoController y cuando creo la lista aplicamdo el Model no llama los métodos que preciso
+	//La otra es que me sugiere hacerlo por constructor, pero no me cierra la idea
+	//Vale la pena hacer un "CategoriaController??" :S
 
 	@GetMapping ("adm")
-	public String getFormProductos() {
+	public String getFormProductos(/*Model model*/) {
+		//model.addAttribute("categorias", "categoria");
 		//productoJpaRepository.jpqlFindByName();
 		return "adm";
 	}
